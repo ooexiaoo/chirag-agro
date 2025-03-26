dcf70a2477882d3237a3d38296f8680d50950a4c
ooexiaoo <vpaherwar@gmail.com>
2025-03-26 20:54:02 +0530

    2025-03-25 - full update

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const map = document.getElementById('map');
+    map.innerHTML = '<iframe width="100%" height="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3692.4392019643124!2d73.17834087529141!3d22.261346779714568!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjLCsDE1JzQwLjkiTiA3M8KwMTAnNTEuMyJF!5e0!3m2!1sen!2sin!4v1742972747019!5m2!1sen!2sin"></iframe>';
+  });
